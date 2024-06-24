16247c1fd914bf0bb64adbbe7c8fec09372b11d4
edivan junior <edivanjuniorfreitas@gmail.com>
2024-06-23 23:27:18 -0300

    FEAT: Tipagem api produto

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
+import { ProductType } from "@/types/ProductType";
 
-export default function Home() {
+export default async function Home() {
+  async function getProducts() {
+    const res = await fetch('https://fakestoreapi.com/products')
+    if (!res.ok) {
+      throw new Error("Falha ao consumir API");
+    }
+    return res.json();
+  }
+  const products = await getProducts();
   return (
-   <div className="max-w-7xl mx-auto border pt-8 px-8 sm:px-0">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        
+    <div className="max-w-7xl mx-auto border pt-8 px-8 sm:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6 text-black">
+        {products.map((product: ProductType) => (
+
+          <div key={product.id}>
+            {product.title}</div>
+        ))}
       </div>
-   </div>
+    </div>
   );
 }
