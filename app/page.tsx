68e299ad034dc61711581f7dba96e2a664fdaff2
edivan junior <edivanjuniorfreitas@gmail.com>
2024-06-23 23:06:05 -0300

    FEAT: Listagem de Produtos

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-      <h1>oii</h1>
+   <div className="max-w-7xl mx-auto border pt-8 px-8 sm:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        
+      </div>
    </div>
   );
 }
